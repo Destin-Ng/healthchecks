f12a1e41a41fdec35e381d93032f94ab7e526a82
hadam94 <hadam@student.bridgew.edu>
2026-04-23 02:27:03 -0400

    Copy filtering buttons to correct place in program

--- manage.py
+++ manage.py
@@ -8,7 +8,6 @@ from django.core.management import execute_from_command_line
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hc.settings")
     logger = logging.getLogger("django")
-
     try:
         execute_from_command_line(sys.argv)
     except Exception as e:
